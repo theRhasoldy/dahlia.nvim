d54008c4ca168517f8cc07ef131b7e1eba3c4197
Nour El Din Shady <rhasoldy@gmail.com>
2023-09-30 13:35:47 +0300

    Oopsie

--- colors/dahlia.lua
+++ colors/dahlia.lua
@@ -126,7 +126,7 @@ colorscheme.highlight_all({
 	SpecialComment = { fg = colors.pumpkin, bold = true, nocombine = true },
 	SpecialKey = "Character",
 	-- InlayHints
-	LSPInlayHints = "Comment",
+	LSPInlayHints = { fg = colors.gray_light, italic = true },
 	-- }}}
 
 	-- Treesitter {{{
